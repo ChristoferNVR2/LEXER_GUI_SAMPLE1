0cdf070cb147a40e6a9d10cdf7f6c490d1ed41b0
chrisvega <cnvegaro@unitru.edu.pe>
2025-02-05 20:24:40 -0500

    Improving the main function

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ extern FILE *yyin;    // Flex uses this file pointer as input
 
 int main(int argc, char *argv[]) {
     if (argc > 1) {
-        // 📌 File input mode
+        // File input mode
         yyin = fopen(argv[1], "r");
         if (!yyin) {
             cerr << "Error: Could not open file " << argv[1] << endl;
@@ -17,13 +17,17 @@ int main(int argc, char *argv[]) {
         }
         cout << "Reading from file: " << argv[1] << endl;
     } else {
-        // 📌 Standard input mode
+        // Standard input mode
         cout << "Enter C++ code (Ctrl+D to end input):\n";
         yyin = stdin;
     }
 
-    // 📌 Start tokenizing
-    while (yylex());
+    // Start tokenizing
+    while (yylex()) {}
+
+    if (yyin != stdin) {
+        fclose(yyin);
+    }
 
     return 0;
 }
